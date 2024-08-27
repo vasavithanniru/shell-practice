becb597e85d1271bf4385f5cd72851baca25455c
vasavi <tvasu370@gmail.com>
2024-08-27 08:05:57 +0530

    special

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash/
 
 echo "all variables passed to the script : $@"
+echo "number of variables passed to the script : $#"
+echo "present cript name : $0"
+echo "user present dir : $PWD"
+echo "user home dir : $HOME"
+echo "current procss instance : $$"
+sleep 10 &
+echo "last process instance of background command: $!"
