2fbef25ea51132f637650e043cec2021bdfed0ca
vasavi <tvasu370@gmail.com>
2025-07-24 12:55:52 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo Hi
+
+PERSON1=Ram
+PERSON2=Raheem
+
+echo "${PERSON1}: Hi ${PERSON2}, how are you!"
+echo "${PERSON2}: Hi ${PERSON1}, I am good. What are you doing?"
+echo "${PERSON1}: I am learning shell script. What abt you?"
+echo "${PERSON2}: I am learning python."
