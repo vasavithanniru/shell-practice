64920355ea724fd2e0f64e1b00d261f6ca682e65
vasavi <tvasu370@gmail.com>
2025-08-02 12:57:07 +0530

    scripts

---
+++ 17.set.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e #setting automatic exit if we get error. instead of exit 1 cmd we can use set -e cmd.set -ex for debug
+
+failure(){
+    echo "failure at $1:$2"
+}        
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR  #ERR is the error signal
+echo "Hi, This is success"
+echoo "Hi, This is failed"
+echo "Hi After fail"
