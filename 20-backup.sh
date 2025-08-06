07fb3491cd728e9b99b2be68e1e468df2eae7c6b
vasavi <tvasu370@gmail.com>
2025-08-06 06:33:45 +0530

    scripts

---
+++ 20-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=$(3:-14) #if $3 is empty, default is 14
+TIMESTAMP=$(date +%F-%H-%M-%S) 
+
+USAGE(){
+    echo "USAGE:: sh 20.backup.sh <source> <destination> <days(optional)>"
+}
+
+if [ $# -lt 2 ]
+then 
+    USAGE
+    exit 1
+fi    
+
+if [ ! -d $SOURCE_DIR ]
+then 
+    echo "$SOURCE_DIR does not exist..Please check"
+    exit 1
+fi  
+
+if [ ! -d $DEST-DIR ]
+then
+    echo "$DEST_DIR does not exist..Please check"
+    exit 1
+fi
+
+FILES=find $SOURCE_DIR -name "*.log" -mtime -$DAYS
+echo "Files: $FILES"
