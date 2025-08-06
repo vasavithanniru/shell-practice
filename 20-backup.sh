ece84f4c1d1ea13fa7a1c99cc07c243885af8c45
vasavi <tvasu370@gmail.com>
2025-08-06 07:42:21 +0530

    scripts

--- 20-backup.sh
+++ 20-backup.sh
@@ -25,5 +25,5 @@ then
     echo "$DEST_DIR does not exist..Please check"
 fi
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime -$DAYS)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files: $FILES"
