a18fc6ea250ca38ebaaab92b273c68441d681031
vasavi <tvasu370@gmail.com>
2025-08-06 07:58:56 +0530

    scripts

--- 20-backup.sh
+++ 20-backup.sh
@@ -27,3 +27,29 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 echo "Files: $FILES"
+
+#Check files are available or not
+if [ ! -z $FILES ]
+then
+    echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app_logs-$TIMESTAMP.zip" #Zip the files
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip $ZIP_FILE -@
+
+    if [ -f $ZIP_FILE]  #check zipping file is succesfull or not
+    then
+        echo "Successfully zipped the files"
+
+        while IFS= read -r file
+        do 
+            echo "Deleting Files: $file"
+            rm -rf $file
+        done <<< $FILES    
+        
+    else
+        echo "Zipping the files is Failed"
+        exit 1    
+    fi
+
+else
+    echo "Files are not found older than $DAYS" 
+fi       
