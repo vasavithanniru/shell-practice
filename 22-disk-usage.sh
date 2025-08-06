fdcff0b2bec360f9283e2e035927491146587626
vasavi <tvasu370@gmail.com>
2025-08-06 15:20:35 +0530

    scripts

---
+++ 22-disk-usage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5   #it is usually 75
+
+while IFS= read -r line
+do 
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    DIR_PATH=$(echo $line | grep xfs | awk -F " " '{print $NF}')
+    if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$DIR_PATH utilized more than $DISK_THRESHOLD. Current value is: $USAGE. Please check..."
+    fi  
+done <<< $DISK_USAGE
