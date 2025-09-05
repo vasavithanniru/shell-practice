ed541fb386aaa5f537b1365412f149ce83ba4e09
vasavi <tvasu370@gmail.com>
2025-09-05 13:30:18 +0530

    script to delete old logs

--- log_cleanup.sh
+++ log_cleanup.sh
@@ -11,7 +11,7 @@ N="\e[0m"    #Reset/Normal
 
 # Log configuration
 LOGS_FOLDER=/var/log/shellscript-logs
-SCRIPT_NAME=$(echo $0 | cud -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%F-%H:%M:%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
 
